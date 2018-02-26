fdd2d4c7f144c85c6a96f307275d74141fc68423
Linx <1808893648@qq.com>
2018-02-26 13:45:24 +0800

    解决无法回答时异常退出问题&&加入了全国2631所高校的名称分词

--- QA/qa.py
+++ QA/qa.py
@@ -10,13 +10,14 @@ import server
 
 
 if(__name__=='__main__'):
-
     mybot = aiml.Kernel()
     server.initQA(mybot)
 
-
     while 1:
         input_message = raw_input("Enter your message >> ")
-        server.QA(input_message,mybot)
+        try:
+            server.QA(input_message,mybot)
+        except:
+            print '很抱歉啦，这个我也母鸡啊！'
 
 
